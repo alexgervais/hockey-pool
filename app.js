a6895d79138e2db51260dd851afb334495d4635f
Alex Gervais <alex.gervais@gmail.com>
2013-04-27 08:42:39 -0400

    imported i18next with jade integration

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path')
+    , i18n = require("i18next");
+
+i18n.init({ lng: 'fr' });
 
 var app = express();
 
@@ -10,24 +13,27 @@ var app = express();
 app.set('port', 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+//app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(i18n.handle);
 app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+app.use(express.cookieParser('a not-so-secret secret key'));
+app.use(express.session());
 app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
+
+i18n.registerAppHelper(app);
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-
 var navbar = [
-    {title: "Home", location:"/"},
-    {title: "Form", location:"/form"}
+    {title: "Home", location: "/"},
+    {title: "Form", location: "/form"}
 ];
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Hockey Pool', navbar: navbar, route: req.route.path });
+exports.index = function (req, res) {
+    res.render('index', { title: 'Hockey Pool', navbar: navbar, route: req.route.path });
 };
