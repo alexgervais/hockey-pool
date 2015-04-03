17624a27f75120cac769e39fdff37a212e3dbfd0
alexgervais <alex.gervais@gmail.com>
2015-04-02 21:40:32 -0400

    changed navbar menu order

--- routes/navbar.js
+++ routes/navbar.js
@@ -12,21 +12,21 @@ module.exports = [
         {key: 'nav.standings', location: '/standings'}
     ]},
     {direction: 'pull-right', navitems: [
-        {key: '2013', class: 'navbar-right', sub: [
-            {key: 'nav.rounds.1', location: '/2013/results/1'},
-            {key: 'nav.rounds.2', location: '/2013/results/2'},
-            {key: 'nav.rounds.3', location: '/2013/results/3'},
-            {key: 'nav.rounds.4', location: '/2013/results/4'},
-            {key: 'nav.standings', location: '/2013/standings'}
-        ]}
-    ]},
-    {direction: 'pull-right', navitems: [
         {key: '2014', class: 'navbar-right', sub: [
             {key: 'nav.rounds.1', location: '/2014/results/1'},
             {key: 'nav.rounds.2', location: '/2014/results/2'},
             {key: 'nav.rounds.3', location: '/2014/results/3'},
             {key: 'nav.rounds.4', location: '/2014/results/4'},
             {key: 'nav.standings', location: '/2014/standings'}
+        ]}
+    ]},
+    {direction: 'pull-right', navitems: [
+        {key: '2013', class: 'navbar-right', sub: [
+            {key: 'nav.rounds.1', location: '/2013/results/1'},
+            {key: 'nav.rounds.2', location: '/2013/results/2'},
+            {key: 'nav.rounds.3', location: '/2013/results/3'},
+            {key: 'nav.rounds.4', location: '/2013/results/4'},
+            {key: 'nav.standings', location: '/2013/standings'}
         ]}
     ]}
 ];
