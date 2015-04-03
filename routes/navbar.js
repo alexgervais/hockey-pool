aeb152631952d5d13093461ece44e5c22699a1df
alexgervais <alex.gervais@gmail.com>
2015-04-02 20:51:33 -0400

    getting ready for 2015 playoffs!

--- routes/navbar.js
+++ routes/navbar.js
@@ -18,4 +18,13 @@ module.exports = [
             {key: "nav.standings", location: "/2013/standings"}
         ]}
     ]},
+    {direction: "pull-right", navitems: [
+        {key: "2014", class: "navbar-right", sub: [
+            {key: "nav.rounds.1", location: "/2014/results/1"},
+            {key: "nav.rounds.2", location: "/2014/results/2"},
+            {key: "nav.rounds.3", location: "/2014/results/3"},
+            {key: "nav.rounds.4", location: "/2014/results/4"},
+            {key: "nav.standings", location: "/2014/standings"}
+        ]}
+    ]}
 ];
