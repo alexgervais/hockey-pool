387c6dd2b105d4fe6aae7b3155d459ba29ed13f1
alexgervais <alex.gervais@gmail.com>
2016-04-09 13:49:10 -0400

    getting ready for 2016 playoffs!

---
+++ config/2015/results.js
@@ -0,0 +1,349 @@
+'use strict';
+
+var roundResults = [];
+roundResults[1] = {
+    roundPoints: 'points.rounds.1',
+    pointScale: 1,
+    result: [
+        ['mtl', 6],
+        ['tbl', 7],
+        ['nyr', 5],
+        ['wsh', 7],
+        ['min', 6],
+        ['chi', 6],
+        ['ana', 4],
+        ['cgy', 6]
+    ],
+    participants: [
+        { name: 'Richard', selections: [
+            ['mtl', 6],
+            ['tbl', 6],
+            ['nyr', 5],
+            ['wsh', 7],
+            ['stl', 6],
+            ['chi', 7],
+            ['ana', 6],
+            ['van', 7]
+        ] },
+        { name: 'André', selections: [
+            ['mtl', 7],
+            ['tbl', 6],
+            ['nyr', 5],
+            ['nyi', 7],
+            ['stl', 7],
+            ['chi', 7],
+            ['ana', 5],
+            ['cgy', 7]
+        ] },
+        { name: 'Johanne', selections: [
+            ['ott', 6],
+            ['tbl', 6],
+            ['nyr', 5],
+            ['wsh', 6],
+            ['stl', 7],
+            ['chi', 6],
+            ['wpg', 7],
+            ['van', 6]
+        ] },
+        { name: 'Mom', selections: [
+            ['mtl', 7],
+            ['tbl', 6],
+            ['nyr', 5],
+            ['nyi', 7],
+            ['stl', 7],
+            ['nsh', 7],
+            ['ana', 5],
+            ['van', 7]
+        ] },
+        { name: 'Alex', selections: [
+            ['ott', 5],
+            ['tbl', 5],
+            ['nyr', 4],
+            ['nyi', 7],
+            ['stl', 6],
+            ['chi', 7],
+            ['ana', 5],
+            ['van', 7]
+        ] },
+        { name: 'Julie', selections: [
+            ['mtl', 6],
+            ['tbl', 7],
+            ['nyr', 5],
+            ['wsh', 7],
+            ['min', 7],
+            ['chi', 6],
+            ['ana', 6],
+            ['van', 7]
+        ] },
+        { name: 'Antoine', selections: [
+            ['mtl', 6],
+            ['tbl', 5],
+            ['nyr', 5],
+            ['nyi', 7],
+            ['stl', 6],
+            ['nsh', 7],
+            ['ana', 5],
+            ['cgy', 7]
+        ] },
+        { name: 'Catherine Roy', selections: [
+            ['mtl', 5],
+            ['tbl', 6],
+            ['pit', 7],
+            ['nyi', 6],
+            ['stl', 5],
+            ['chi', 5],
+            ['ana', 4],
+            ['van', 5]
+        ] },
+        { name: 'e1000', selections: [
+            ['mtl', 6],
+            ['tbl', 4],
+            ['nyr', 4],
+            ['nyi', 6],
+            ['stl', 5],
+            ['chi', 6],
+            ['ana', 5],
+            ['cgy', 7]
+        ] },
+        { name: 'Catherine Rousseau', selections: [
+            ['mtl', 5],
+            ['det', 6],
+            ['nyr', 6],
+            ['nyi', 7],
+            ['stl', 7],
+            ['chi', 5],
+            ['ana', 6],
+            ['van', 7]
+        ] },
+        { name: 'Éliane', selections: [
+            ['mtl', 6],
+            ['tbl', 5],
+            ['nyr', 7],
+            ['wsh', 5],
+            ['stl', 6],
+            ['chi', 6],
+            ['ana', 4],
+            ['van', 6]
+        ] },
+        { name: 'Alexandre G-P', selections: [
+            ['mtl', 5],
+            ['tbl', 5],
+            ['nyr', 5],
+            ['wsh', 6],
+            ['stl', 6],
+            ['chi', 6],
+            ['ana', 5],
+            ['van', 7]
+        ] },
+        { name: 'Martin', selections: [
+            ['ott', 6],
+            ['tbl', 5],
+            ['nyr', 6],
+            ['nyi', 7],
+            ['stl', 6],
+            ['chi', 7],
+            ['ana', 5],
+            ['cgy', 5]
+        ] }
+    ]
+};
+
+roundResults[2] = {
+    roundPoints: 'points.rounds.2',
+    pointScale: 2,
+    result: [
+        ['tbl', 6],
+        ['nyr', 7],
+        ['chi', 4],
+        ['ana', 5]
+    ],
+    participants: [
+        { name: 'Richard', selections: [
+            ['mtl', 7],
+            ['nyr', 5],
+            ['chi', 6],
+            ['cgy', 7]
+        ] },
+        { name: 'André', selections: [
+            ['mtl', 7],
+            ['nyr', 5],
+            ['min', 6],
+            ['ana', 5]
+        ] },
+        { name: 'Johanne', selections: [
+            ['tbl', 6],
+            ['nyr', 6],
+            ['chi', 6],
+            ['ana', 6]
+        ] },
+        { name: 'Mom', selections: [
+            ['mtl', 7],
+            ['nyr', 5],
+            ['min', 6],
+            ['ana', 5]
+        ] },
+        { name: 'Alex', selections: [
+            ['mtl', 6],
+            ['nyr', 5],
+            ['chi', 7],
+            ['ana', 6]
+        ] },
+        { name: 'Julie', selections: [
+            ['mtl', 7],
+            ['nyr', 6],
+            ['chi', 7],
+            ['ana', 5]
+        ] },
+        { name: 'Antoine', selections: [
+            ['mtl', 6],
+            ['nyr', 5],
+            ['min', 7],
+            ['ana', 5]
+        ] },
+        { name: 'Catherine Roy', selections: [
+            ['mtl', 6],
+            ['nyr', 6],
+            ['min', 6],
+            ['ana', 6]
+        ] },
+        { name: 'e1000', selections: [
+            ['mtl', 6],
+            ['nyr', 5],
+            ['chi', 6],
+            ['ana', 5]
+        ] },
+        { name: 'Catherine Rousseau', selections: [
+            ['mtl', 6],
+            ['nyr', 7],
+            ['chi', 5],
+            ['ana', 6]
+        ] },
+        { name: 'Éliane', selections: [
+            ['mtl', 6],
+            ['nyr', 7],
+            ['chi', 5],
+            ['ana', 6]
+        ] },
+        { name: 'Alexandre G-P', selections: [
+
+        ] },
+        { name: 'Martin', selections: [
+            ['tbl', 5],
+            ['nyr', 4],
+            ['chi', 7],
+            ['ana', 5]
+        ] }
+    ]
+};
+
+roundResults[3] = {
+    roundPoints: 'points.rounds.3',
+    pointScale: 3,
+    result: [
+        ['tbl', 7],
+        ['chi', 7]
+    ],
+    participants: [
+        { name: 'Richard', selections: [
+            ['nyr', 7],
+            ['ana', 7]
+        ] },
+        { name: 'André', selections: [
+            ['tbl', 6],
+            ['chi', 6]
+        ] },
+        { name: 'Johanne', selections: [
+            ['tbl', 7],
+            ['chi', 7]
+        ] },
+        { name: 'Mom', selections: [
+            ['nyr', 6],
+            ['chi', 7]
+        ] },
+        { name: 'Alex', selections: [
+            ['nyr', 6],
+            ['chi', 7]
+        ] },
+        { name: 'Julie', selections: [
+            ['nyr', 6],
+            ['chi', 7]
+        ] },
+        { name: 'Antoine', selections: [
+            ['tbl', 6],
+            ['ana', 6]
+        ] },
+        { name: 'Catherine Roy', selections: [
+            ['nyr', 6],
+            ['chi', 7]
+        ] },
+        { name: 'e1000', selections: [
+            ['nyr', 7],
+            ['ana', 7]
+        ] },
+        { name: 'Catherine Rousseau', selections: [
+            ['tbl', 6],
+            ['chi', 5]
+        ] },
+        { name: 'Éliane', selections: [
+
+        ] },
+        { name: 'Alexandre G-P', selections: [
+
+        ] },
+        { name: 'Martin', selections: [
+            ['nyr', 6],
+            ['ana', 6]
+        ] }
+    ]
+};
+
+roundResults[4] = {
+    roundPoints: 'points.rounds.4',
+    pointScale: 4,
+    result: [
+        ['chi', 6]
+    ],
+    participants: [
+        { name: 'Richard', selections: [
+            ['chi', 6]
+        ] },
+        { name: 'André', selections: [
+            ['tbl', 7]
+        ] },
+        { name: 'Johanne', selections: [
+            ['chi', 6]
+        ] },
+        { name: 'Mom', selections: [
+            ['tbl', 6]
+        ] },
+        { name: 'Alex', selections: [
+            ['chi', 5]
+        ] },
+        { name: 'Julie', selections: [
+            ['chi', 5]
+        ] },
+        { name: 'Antoine', selections: [
+            ['chi', 5]
+        ] },
+        { name: 'Catherine Roy', selections: [
+
+        ] },
+        { name: 'e1000', selections: [
+            ['chi', 6]
+        ] },
+        { name: 'Catherine Rousseau', selections: [
+
+        ] },
+        { name: 'Éliane', selections: [
+
+        ] },
+        { name: 'Alexandre G-P', selections: [
+
+        ] },
+        { name: 'Martin', selections: [
+            ['tbl', 5]
+        ] }
+    ]
+};
+
+module.exports = roundResults;
---
+++ config/2015/standings.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    participants: [
+        {name: 'Richard', points: [9, 4, 0, 8]},
+        {name: 'André', points: [7, 6, 6, 0]},
+        {name: 'Johanne', points: [6, 10, 12, 8]},
+        {name: 'Mom', points: [5, 6, 6, 0]},
+        {name: 'Alex', points: [4, 6, 6, 4]},
+        {name: 'Julie', points: [12, 8, 6, 4]},
+        {name: 'Antoine', points: [7, 6, 3, 4]},
+        {name: 'Catherine Roy', points: [5, 4, 6, 0]},
+        {name: 'E1000', points: [8, 8, 0, 8]},
+        {name: 'Catherine Rousseau', points: [4, 8, 6, 0]},
+        {name: 'Éliane', points: [9, 8, 0, 0]},
+        {name: 'Alexandre G-P', points: [8, 0, 0, 0]},
+        {name: 'Martin', points: [5, 10, 0, 0]}
+    ]
+};
