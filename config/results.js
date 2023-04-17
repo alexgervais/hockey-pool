ccaeae91c45031648938d6fff65b77d51df9712a
alexgervais <alex.gervais@gmail.com>
2023-04-17 19:04:21 -0400

    Round 1 picks

--- config/results.js
+++ config/results.js
@@ -1,12 +1,155 @@
 'use strict';
 
 var roundResults = [];
-roundResults[0] = {
+roundResults[1] = {
     roundPoints: 'points.rounds.1',
     pointScale: 1,
-    result: [
-    ],
+    result: [],
     participants: [
+        {
+            name: 'Alex', selections: [
+                ['bos', 5],
+                ['tbl', 7],
+                ['car', 4],
+                ['nyr', 7],
+                ['col', 6],
+                ['dal', 7],
+                ['vgk', 4],
+                ['edm', 7]
+            ]
+        },
+        {
+            name: 'Eliane', selections: [
+                ['bos', 5],
+                ['tor', 5],
+                ['car', 5],
+                ['njd', 5],
+                ['col', 5],
+                ['dal', 5],
+                ['vgk', 5],
+                ['edm', 5]
+            ]
+        },
+        {
+            name: 'Ophélia', selections: [
+                ['fla', 6],
+                ['tbl', 6],
+                ['nyi', 6],
+                ['nyr', 6],
+                ['sea', 6],
+                ['min', 6],
+                ['wpg', 6],
+                ['lak', 6]
+            ]
+        },
+        {
+            name: 'Quentin', selections: [
+                ['bos', 4],
+                ['tbl', 7],
+                ['car', 4],
+                ['nyr', 7],
+                ['col', 4],
+                ['min', 7],
+                ['vgk', 4],
+                ['lak', 7]
+            ]
+        },
+        {
+            name: 'Marie-Christine', selections: [
+                ['bos', 7],
+                ['tor', 7],
+                ['car', 7],
+                ['njd', 7],
+                ['col', 7],
+                ['dal', 7],
+                ['vgk', 7],
+                ['edm', 7]
+            ]
+        },
+        {
+            name: 'Antoine', selections: [
+                ['bos', 4],
+                ['tbl', 6],
+                ['car', 5],
+                ['nyr', 6],
+                ['col', 5],
+                ['dal', 6],
+                ['vgk', 5],
+                ['edm', 7]
+            ]
+        },
+        {
+            name: 'André', selections: [
+                ['bos', 5],
+                ['tbl', 7],
+                ['car', 7],
+                ['nyr', 6],
+                ['col', 5],
+                ['dal', 7],
+                ['vgk', 6],
+                ['edm', 5]
+            ]
+        },
+        {
+            name: 'Mom', selections: [
+                ['bos', 4],
+                ['tbl', 5],
+                ['car', 6],
+                ['njd', 6],
+                ['col', 5],
+                ['dal', 6],
+                ['vgk', 5],
+                ['edm', 7]
+            ]
+        },
+        {
+            name: 'Richard', selections: [
+                ['bos', 5],
+                ['tor', 6],
+                ['car', 6],
+                ['nyr', 7],
+                ['col', 5],
+                ['dal', 7],
+                ['vgk', 6],
+                ['edm', 6]
+            ]
+        },
+        {
+            name: 'Martin', selections: [
+                ['bos', 5],
+                ['tbl', 6],
+                ['nyi', 7],
+                ['nyr', 6],
+                ['col', 6],
+                ['dal', 6],
+                ['wpg', 5],
+                ['edm', 6]
+            ]
+        },
+        {
+            name: 'Johanne', selections: [
+                ['bos', 5],
+                ['tor', 6],
+                ['car', 6],
+                ['nyr', 7],
+                ['col', 5],
+                ['dal', 7],
+                ['wpg', 6],
+                ['edm', 6]
+            ]
+        },
+        {
+            name: 'Alex G-P', selections: [
+                ['bos', 5],
+                ['tbl', 6],
+                ['nyi', 7],
+                ['nyr', 4],
+                ['col', 5],
+                ['dal', 6],
+                ['vgk', 4],
+                ['edm', 4]
+            ]
+        },
     ]
 };
 
