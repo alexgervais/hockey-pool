3cc42db30e2b91906b48d3d9d9a5d533023d5cfd
Alex Gervais <alex.gervais@gmail.com>
2023-05-01 22:41:32 -0400

    Update registration.js

--- config/registration.js
+++ config/registration.js
@@ -3,36 +3,32 @@
 var moment = require('moment');
 
 module.exports = {
-    closingOn: moment('2023-04-17 19:01 -0400', 'YYYY-MM-DD HH:mm ZZ'),
-    round: 'nav.rounds.1',
-    roundPoints: 'points.rounds.1',
+    closingOn: moment('2023-05-02 19:00 -0400', 'YYYY-MM-DD HH:mm ZZ'),
+    round: 'nav.rounds.2',
+    roundPoints: 'points.rounds.2',
     predictions: [
         {
             section: 'atlantic',
             confrontations: [
-                ['bos', 'fla'],
-                ['tor', 'tbl']
+                ['fla', 'tor']
             ]
         },
         {
             section: 'metropolitan',
             confrontations: [
-                ['car', 'nyi'],
-                ['njd', 'nyr']
+                ['car', 'njd']
             ]
         },
         {
             section: 'central',
             confrontations: [
-                ['col', 'sea'],
-                ['dal', 'min']
+                ['sea', 'dal']
             ]
         },
         {
             section: 'pacific',
             confrontations: [
-                ['vgk', 'wpg'],
-                ['edm', 'lak']
+                ['vgk', 'edm']
             ]
         }
     ]
