bdfdfb5f3913530775132ea89b411c94487f1088
Alex Gervais <alex.gervais@gmail.com>
2023-05-16 08:44:30 +0200

    Update standings.js

--- config/standings.js
+++ config/standings.js
@@ -2,17 +2,17 @@
 
 module.exports = {
     participants: [
-        {name: 'Alex', points: [4]},
-        {name: 'Alex G-P', points: [4]},
-        {name: 'André', points: [4]},
-        {name: 'Antoine', points: [6]},
-        {name: 'Eliane', points: [7]},
-        {name: 'Johanne', points: [7]},
-        {name: 'Marie-Christine', points: [7]},
-        {name: 'Martin', points: [4]},
-        {name: 'Mom', points: [8]},
-        {name: 'Ophélia', points: [2]},
-        {name: 'Quentin', points: [2]},
-        {name: 'Richard', points: [8]}
+        {name: 'Alex', points: [4, 6]},
+        {name: 'Alex G-P', points: [4, 10]},
+        {name: 'André', points: [4, 4]},
+        {name: 'Antoine', points: [6, 4]},
+        {name: 'Eliane', points: [7, 4]},
+        {name: 'Johanne', points: [7, 2]},
+        {name: 'Marie-Christine', points: [7, 6]},
+        {name: 'Martin', points: [4, 6]},
+        {name: 'Mom', points: [8, 6]},
+        {name: 'Ophélia', points: [2, 8]},
+        {name: 'Quentin', points: [2, 4]},
+        {name: 'Richard', points: [8, 4]}
     ]
 };
