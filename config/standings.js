0cbce5910654410616caafa0ddb0646c1af9c336
Alex Gervais <alex.gervais@gmail.com>
2023-06-13 20:03:37 -0700

    Update standings.js

--- config/standings.js
+++ config/standings.js
@@ -2,17 +2,17 @@
 
 module.exports = {
     participants: [
-        {name: 'Alex', points: [4, 6, 9]},
-        {name: 'Alex G-P', points: [4, 10, 3]},
-        {name: 'André', points: [4, 4, 9]},
-        {name: 'Antoine', points: [6, 4, 6]},
-        {name: 'Eliane', points: [7, 4, 9]},
-        {name: 'Johanne', points: [7, 2, 0]},
-        {name: 'Marie-Christine', points: [7, 6, 3]},
-        {name: 'Martin', points: [4, 6, 0]},
-        {name: 'Mom', points: [8, 6, 6]},
-        {name: 'Ophélia', points: [2, 8, 0]},
-        {name: 'Quentin', points: [2, 4, 0]},
-        {name: 'Richard', points: [8, 4, 6]}
+        {name: 'Alex', points: [4, 6, 9, 8]},
+        {name: 'Alex G-P', points: [4, 10, 3, 0]},
+        {name: 'André', points: [4, 4, 9, 0]},
+        {name: 'Antoine', points: [6, 4, 6, 0]},
+        {name: 'Eliane', points: [7, 4, 9, 8]},
+        {name: 'Johanne', points: [7, 2, 0, 8]},
+        {name: 'Marie-Christine', points: [7, 6, 3, 0]},
+        {name: 'Martin', points: [4, 6, 0, 0]},
+        {name: 'Mom', points: [8, 6, 6, 4]},
+        {name: 'Ophélia', points: [2, 8, 0, 0]},
+        {name: 'Quentin', points: [2, 4, 0, 0]},
+        {name: 'Richard', points: [8, 4, 6, 8]}
     ]
 };
