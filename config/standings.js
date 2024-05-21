6e4f71da81c331c5cd0188138a2ed75084ce2dca
alexgervais <alex.gervais@gmail.com>
2024-05-21 09:07:22 -0400

    Round 2 standings update

--- config/standings.js
+++ config/standings.js
@@ -2,15 +2,15 @@
 
 module.exports = {
     participants: [
-        {name: 'Alex', points: [10]},
-        {name: 'Alexandre G-P', points: [7]},
-        {name: 'André', points: [9]},
-        {name: 'Antoine', points: [10]},
-        {name: 'Eliane', points: [8]},
-        {name: 'Johanne', points: [9]},
-        {name: 'Marie-Christine', points: [9]},
-        {name: 'Martin', points: [9]},
-        {name: 'Mom', points: [8]},
-        {name: 'Richard', points: [8]}
+        {name: 'Alex', points: [10, 6]},
+        {name: 'Alexandre G-P', points: [7, 8]},
+        {name: 'André', points: [9, 10]},
+        {name: 'Antoine', points: [10, 10]},
+        {name: 'Eliane', points: [8, 8]},
+        {name: 'Johanne', points: [9, 8]},
+        {name: 'Marie-Christine', points: [9, 4]},
+        {name: 'Martin', points: [9, 8]},
+        {name: 'Mom', points: [8, 10]},
+        {name: 'Richard', points: [8, 8]}
     ]
 };
