a8e6c8d0d77372636c3c59d1601ab5fc47b863f8
alexgervais <alex.gervais@gmail.com>
2025-04-19 23:27:58 -0400

    Round 1 predictions

--- config/results.js
+++ config/results.js
@@ -10,16 +10,106 @@ roundResults[0] = {
         // ['tor', 5]
     ],
     participants: [
-        /*{ name: 'Richard', selections: [
-            ['pit', 5],
-            ['mtl', 6],
-            ['nyr', 7],
-            ['tor', 7],
-            ['chi', 4],
-            ['det', 7],
-            ['sjs', 6],
+        { name: 'Alex', selections: [
+            ['tor', 6],
+            ['fla', 7],
+            ['mtl', 7],
+            ['car', 6],
+            ['wpg', 4],
+            ['col', 7],
+            ['vgk', 5],
+            ['edm', 6]
+        ] },
+        { name: 'Martin', selections: [
+            ['tor', 5],
+            ['fla', 6],
+            ['mtl', 7],
+            ['car', 6],
+            ['wpg', 5],
+            ['col', 7],
+            ['vgk', 6],
+            ['edm', 7]
+        ] },
+        { name: 'Johanne', selections: [
+            ['tor', 6],
+            ['tbl', 6],
+            ['wsh', 6],
+            ['car', 5],
+            ['wpg', 7],
+            ['col', 6],
+            ['vgk', 5],
             ['lak', 6]
-        ] },*/
+        ] },
+        { name: 'Eliane', selections: [
+            ['tor', 6],
+            ['fla', 7],
+            ['wsh', 5],
+            ['car', 6],
+            ['wpg', 5],
+            ['dal', 6],
+            ['vgk', 5],
+            ['edm', 6]
+        ] },
+        { name: 'Antoine', selections: [
+            ['tor', 6],
+            ['fla', 6],
+            ['mtl', 6],
+            ['car', 5],
+            ['wpg', 5],
+            ['dal', 7],
+            ['vgk', 6],
+            ['edm', 6]
+        ] },
+        { name: 'Richard', selections: [
+            ['ott', 7],
+            ['tbl', 6],
+            ['wsh', 6],
+            ['car', 6],
+            ['wpg', 5],
+            ['col', 7],
+            ['vgk', 6],
+            ['lak', 6]
+        ] },
+        { name: 'André', selections: [
+            ['ott', 7],
+            ['fla', 6],
+            ['mtl', 6],
+            ['car', 5],
+            ['wpg', 5],
+            ['col', 6],
+            ['vgk', 5],
+            ['edm', 6]
+        ] },
+        { name: 'Mom', selections: [
+            ['ott', 6],
+            ['fla', 5],
+            ['mtl', 7],
+            ['car', 7],
+            ['wpg', 5],
+            ['dal', 6],
+            ['vgk', 6],
+            ['lak', 7]
+        ] },
+        { name: 'Marie-Christine', selections: [
+            ['ott', 4],
+            ['fla', 6],
+            ['wsh', 6],
+            ['njd', 4],
+            ['wpg', 7],
+            ['col', 6],
+            ['vgk', 5],
+            ['edm', 4]
+        ] },
+        { name: 'Alex G P', selections: [
+            ['ott', 6],
+            ['fla', 5],
+            ['mtl', 7],
+            ['car', 6],
+            ['wpg', 5],
+            ['col', 5],
+            ['min', 5],
+            ['edm', 6]
+        ] },
     ]
 };
 
