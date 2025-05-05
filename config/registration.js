83de6e9d14f1fe8bc4e38de7cac7825ddd48b69a
alexgervais <alex.gervais@gmail.com>
2025-05-05 08:36:41 -0400

    r2 registration fix

--- config/registration.js
+++ config/registration.js
@@ -4,10 +4,10 @@ var moment = require('moment');
 
 module.exports = {
     storageBucket: 'misezsurlaglace-predictions',
-    storageFolder: '/2025/1',
+    storageFolder: '/2025/2',
     closingOn: moment('2025-05-05 20:00 -0400', 'YYYY-MM-DD HH:mm ZZ'),
-    round: 'nav.rounds.1',
-    roundPoints: 'points.rounds.1',
+    round: 'nav.rounds.2',
+    roundPoints: 'points.rounds.2',
     predictions: [
         {
             section: 'atlantic',
