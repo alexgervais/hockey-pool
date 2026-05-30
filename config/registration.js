da25dfe7ddef16db289e0d9ceb069d153b3b0814
Alex Gervais <alex.gervais@gmail.com>
2026-05-29 23:02:54 -0400

    Update registration.js

--- config/registration.js
+++ config/registration.js
@@ -4,21 +4,15 @@ var moment = require('moment');
 
 module.exports = {
     storageBucket: 'misezsurlaglace-predictions',
-    storageFolder: '/2026/3',
-    closingOn: moment('2026-05-20 20:00 -0400', 'YYYY-MM-DD HH:mm ZZ'),
-    round: 'nav.rounds.3',
-    roundPoints: 'points.rounds.3',
+    storageFolder: '/2026/4',
+    closingOn: moment('2026-06-02 20:00 -0400', 'YYYY-MM-DD HH:mm ZZ'),
+    round: 'nav.rounds.4',
+    roundPoints: 'points.rounds.4',
     predictions: [
         {
-            section: 'east',
+            section: 'final',
             confrontations: [
-                ['car', 'mtl']
-            ]
-        },
-        {
-            section: 'west',
-            confrontations: [
-                ['col', 'vgk']
+                ['car', 'vgk']
             ]
         }
     ]
